76a733f35fd9ab659eaf66384dde85cc1faaaacb
Ricardo Delfin <me@rdelfin.com>
2021-02-19 12:21:53 +0000

    Switched publisher to send images

--- src/bin/pub.rs
+++ src/bin/pub.rs
@@ -1,29 +1,71 @@
 use prost::Message;
-use rand::{
-    distributions::{Distribution, Uniform},
-    thread_rng,
-};
+use std::{error, fs, path::PathBuf, time::SystemTime};
+use structopt::StructOpt;
 use zeromq_test::data::{Action, Image};
 use zmq::Context;
 
-fn main() {
+/// A basic example
+#[derive(StructOpt, Debug)]
+#[structopt(name = "image_pub")]
+struct Opt {
+    #[structopt(short = "p", long)]
+    image_path: PathBuf,
+
+    #[structopt(short, long)]
+    extension: String,
+}
+
+fn main() -> Result<(), Box<dyn error::Error>> {
+    let opt = Opt::from_args();
     let context = Context::new();
     let publisher = context.socket(zmq::PUB).unwrap();
 
-    assert!(publisher.bind("tcp://*:5556").is_ok());
-    assert!(publisher.bind("ipc://action.ipc").is_ok());
+    assert!(publisher.bind("ipc://camera.ipc").is_ok());
 
-    let mut rng = rand::thread_rng();
-    let angle_dist = Uniform::new(0.0, 2. * std::f64::consts::PI);
-    let speed_dist = Uniform::new(0.0, 5. as f64);
+    let images = load_images(opt.image_path, &opt.extension)?;
+    let mut idx = 0;
 
     loop {
-        let action = Action {
-            angle: angle_dist.sample(&mut rng),
-            speed: speed_dist.sample(&mut rng),
-        };
+        idx = (idx + 1) % images.len();
+        let ts = SystemTime::now()
+            .duration_since(SystemTime::UNIX_EPOCH)?
+            .as_nanos() as u64;
+
+        let image = image_from_data(&images[idx], ts);
         let mut buf: Vec<u8> = Vec::new();
-        action.encode(&mut buf).unwrap();
+        image.encode(&mut buf).unwrap();
         publisher.send(&buf, 0).unwrap();
+    }
+}
+
+fn load_images(dir: PathBuf, ext: &str) -> Result<Vec<Vec<u8>>, Box<dyn error::Error>> {
+    Ok(fs::read_dir(dir.as_path())?
+        .filter(|e| match e {
+            Ok(entry) => {
+                let is_file = {
+                    match entry.file_type() {
+                        Ok(ftype) => ftype.is_file(),
+                        Err(_) => false,
+                    }
+                };
+                let correct_ext = { entry.path().extension().unwrap() == ext };
+                is_file && correct_ext
+            }
+            Err(_) => false,
+        })
+        .map(|e| fs::read(e.unwrap().path().as_path()))
+        .collect::<Result<Vec<_>, _>>()?
+        .iter()
+        .map(|data| data[32..].into())
+        .collect())
+}
+
+fn image_from_data(data: &Vec<u8>, ts: u64) -> Image {
+    Image {
+        timestamp: ts,
+        width: 2048,
+        height: 1280,
+        channels: 3,
+        data: data.clone(),
     }
 }

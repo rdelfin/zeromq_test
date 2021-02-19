990fa9d54e5bd3e6d58f5be44e0e95e11751a8cf
Ricardo Delfin <me@rdelfin.com>
2021-02-19 23:36:01 +0000

    Added tonic dependency

--- build.rs
+++ build.rs
@@ -1,7 +1,7 @@
 use std::error;
 
 fn main() -> Result<(), Box<dyn error::Error>> {
-    prost_build::compile_protos(&["proto/data.proto"], &["proto/"])?;
+    tonic_build::compile_protos("proto/data.proto")?;
     ::capnpc::CompilerCommand::new()
         .file("capnp/data.capnp")
         .run()
